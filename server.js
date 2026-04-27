a54311a8c8bc8bc9437a7c49c431afe3374c17c1
pro628713-arch <pro628713@gmail.com>
2026-04-27 08:10:41 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,13 +5,26 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// قائمة الأدمن
+const admins = ["adam_saadi8737", "go2mohamed"];
+
 let messages = [];
 
 app.post('/send', (req, res) => {
     const { username, message } = req.body;
     if (!username || !message) return res.status(400).json({ error: 'Missing' });
-    messages.push({ id: Date.now(), username, message });
+
+    const isAdmin = admins.includes(username);
+
+    messages.push({
+        id: Date.now(),
+        username,
+        message,
+        isAdmin
+    });
+
     if (messages.length > 50) messages.shift();
+
     res.json({ success: true });
 });
 
